dce06997899f10b39b88fd79d655e181f208df5f
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-16 22:06:24 +0200

    Suppress category fetch error display and add tests for product and category fetch error handling.

--- tests/pages/BrowseProductsPage.test.tsx
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -54,4 +54,27 @@ describe("BrowseProductsPages", () => {
       screen.getByRole("progressbar", { name: /products/i })
     ).toBeInTheDocument();
   });
+
+  it("should not render an error if categories cannot be fetched", async () => {
+    server.use(http.get("/categories", () => HttpResponse.error()));
+
+    renderComponent();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole("progressbar", { name: /categories/i })
+    );
+
+    expect(screen.queryByText(/error/i)).not.toBeInTheDocument();
+    expect(
+      screen.queryByRole("combobox", { name: /category/i })
+    ).not.toBeInTheDocument();
+  });
+
+  it("should render an error if products can not be fetched", async () => {
+    server.use(http.get("/products", () => HttpResponse.error()));
+
+    renderComponent();
+
+    expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
 });
