79ff8274e17bd875a2a6d2fb894ae127538b468d
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-16 22:16:49 +0200

    Add category mocking and handlers, and update `BrowseProductsPage` tests to render categories and products.

--- tests/pages/BrowseProductsPage.test.tsx
+++ tests/pages/BrowseProductsPage.test.tsx
@@ -7,13 +7,41 @@ import { server } from "../mocks/server";
 import { http, delay, HttpResponse } from "msw";
 import BrowseProducts from "../../src/pages/BrowseProductsPage";
 import { Theme } from "@radix-ui/themes";
+import userEvent from "@testing-library/user-event";
+import { db } from "../mocks/db";
+import { Category, Product } from "../../src/entities";
+import { CartProvider } from "../../src/providers/CartProvider";
 
-describe("BrowseProductsPages", () => {
+describe("BrowseProductsPage", () => {
+  const categories: Category[] = [];
+  const products: Product[] = [];
+
+  beforeAll(() => {
+    [1, 2].forEach(() => {
+      categories.push(db.category.create());
+      products.push(db.product.create());
+    });
+  });
+
+  afterAll(() => {
+    const categoryIds = categories.map((c) => c.id);
+    db.category.deleteMany({
+      where: { id: { in: categoryIds } },
+    });
+
+    const productIds = products.map((p) => p.id);
+    db.product.deleteMany({
+      where: { id: { in: productIds } },
+    });
+  });
+
   const renderComponent = () => {
     render(
-      <Theme>
-        <BrowseProducts />
-      </Theme>
+      <CartProvider>
+        <Theme>
+          <BrowseProducts />
+        </Theme>
+      </CartProvider>
     );
   };
 
@@ -40,7 +68,7 @@ describe("BrowseProductsPages", () => {
     );
   });
 
-  it("should hide the loading skeleton after products are fetched", () => {
+  it("should show a loading skeleton when fetching products", () => {
     server.use(
       http.get("/products", async () => {
         await delay();
@@ -53,6 +81,14 @@ describe("BrowseProductsPages", () => {
     expect(
       screen.getByRole("progressbar", { name: /products/i })
     ).toBeInTheDocument();
+  });
+
+  it("should hide the loading skeleton after products are fetched", async () => {
+    renderComponent();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole("progressbar", { name: /products/i })
+    );
   });
 
   it("should not render an error if categories cannot be fetched", async () => {
@@ -70,11 +106,40 @@ describe("BrowseProductsPages", () => {
     ).not.toBeInTheDocument();
   });
 
-  it("should render an error if products can not be fetched", async () => {
+  it("should render an error if products cannot be fetched", async () => {
     server.use(http.get("/products", () => HttpResponse.error()));
 
     renderComponent();
 
     expect(await screen.findByText(/error/i)).toBeInTheDocument();
+  });
+
+  it("should render categories", async () => {
+    renderComponent();
+
+    const combobox = await screen.findByRole("combobox");
+    expect(combobox).toBeInTheDocument();
+
+    const user = userEvent.setup();
+    await user.click(combobox);
+
+    expect(screen.getByRole("option", { name: /all/i })).toBeInTheDocument();
+    categories.forEach((category) => {
+      expect(
+        screen.getByRole("option", { name: category.name })
+      ).toBeInTheDocument();
+    });
+  });
+
+  it("should render products", async () => {
+    renderComponent();
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole("progressbar", { name: /products/i })
+    );
+
+    products.forEach((product) => {
+      expect(screen.getByText(product.name)).toBeInTheDocument();
+    });
   });
 });
