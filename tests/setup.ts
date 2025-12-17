fec042b5a31b53358ed901c059ef3ce577ce402f
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-17 16:13:36 +0200

    mock Auth0 in setup and add a utility to control its authentication state

--- tests/setup.ts
+++ tests/setup.ts
@@ -3,9 +3,10 @@ import ResizeObserver from "resize-observer-polyfill";
 import { server } from './mocks/server';
 
 beforeAll(() => server.listen());
-
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+vi.mock("@auth0/auth0-react");
 
 globalThis.ResizeObserver = ResizeObserver;
 
