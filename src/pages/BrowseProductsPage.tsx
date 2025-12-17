4fc5e75b64adad763f859e46e9938d1ab9ab5776
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2025-12-17 09:40:37 +0200

    extract category selection logic into a new `CategorySelect` component

--- src/pages/BrowseProductsPage.tsx
+++ src/pages/BrowseProductsPage.tsx
@@ -1,76 +1,34 @@
-import { Select, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import axios from "axios";
 import { useState } from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
 import { useQuery } from "react-query";
+import CategorySelect from "../components/CategorySelect";
 import QuantitySelector from "../components/QuantitySelector";
-import { Category, Product } from "../entities";
+import { Product } from "../entities";
 
 function BrowseProducts() {
-  const categoriesQuery = useQuery({
-    queryKey: ['categories'],
-    queryFn: () => axios.get<Category[]>('/categories').then(res => res.data)
+  const productsQuery = useQuery<Product[], Error>({
+    queryKey: ["products"],
+    queryFn: () => axios.get<Product[]>("/products").then((res) => res.data),
   });
 
-  const productsQuery = useQuery<Product[], Error>({
-    queryKey: ['products'],
-    queryFn: () => axios.get<Product[]>('/products').then(res => res.data)
-  });
-  
   const [selectedCategoryId, setSelectedCategoryId] = useState<
     number | undefined
   >();
 
-  if (productsQuery.error) return <div>Error: {productsQuery.error.message}</div>;
-
-  const renderCategories = () => {
-    const { isLoading, error, data: categories } = categoriesQuery;
-
-    if (isLoading)
-      return (
-        <div role="progressbar" aria-label="Loading categories">
-          <Skeleton />
-        </div>
-      );
-
-    if (error)  return null;
-
-    return (
-      <Select.Root
-        onValueChange={(categoryId) =>
-          setSelectedCategoryId(parseInt(categoryId))
-        }
-      >
-        <Select.Trigger placeholder="Filter by Category" />
-        <Select.Content>
-          <Select.Group>
-            <Select.Label>Category</Select.Label>
-            <Select.Item value="all">All</Select.Item>
-            {categories?.map((category) => (
-              <Select.Item
-                key={category.id}
-                value={category.id.toString()}
-              >
-                {category.name}
-              </Select.Item>
-            ))}
-          </Select.Group>
-        </Select.Content>
-      </Select.Root>
-    );
-  };
+  if (productsQuery.error)
+    return <div>Error: {productsQuery.error.message}</div>;
 
   const renderProducts = () => {
     const skeletons = [1, 2, 3, 4, 5];
-    const {error, data: products, isLoading} = productsQuery;
+    const { error, data: products, isLoading } = productsQuery;
 
     if (error) return <div>Error: {error.message}</div>;
 
     const visibleProducts = selectedCategoryId
-      ? products!.filter(
-          (p) => p.categoryId === selectedCategoryId
-        )
+      ? products!.filter((p) => p.categoryId === selectedCategoryId)
       : products;
 
     return (
@@ -78,15 +36,13 @@ function BrowseProducts() {
         <Table.Header>
           <Table.Row>
             <Table.ColumnHeaderCell>Name</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>
-              Price
-            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell>Price</Table.ColumnHeaderCell>
             <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
           </Table.Row>
         </Table.Header>
         <Table.Body
           role={isLoading ? "progressbar" : undefined}
-          aria-label={isLoading ? 'Loading products' : undefined}
+          aria-label={isLoading ? "Loading products" : undefined}
         >
           {isLoading &&
             skeletons.map((skeleton) => (
@@ -120,7 +76,11 @@ function BrowseProducts() {
   return (
     <div>
       <h1>Products</h1>
-      <div className="max-w-xs">{renderCategories()}</div>
+      <div className="max-w-xs">
+        <CategorySelect
+          onChange={(categoryId) => setSelectedCategoryId(categoryId)}
+        />
+      </div>
       {renderProducts()}
     </div>
   );
